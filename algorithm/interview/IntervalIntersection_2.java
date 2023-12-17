eff95b5c847544e4c70c8e7663644f6b0d38855d
ban <lubban@163.com>
2023-12-17 15:14:22 +0800

    区间问题

--- algorithm/interview/IntervalIntersection_2.java
+++ algorithm/interview/IntervalIntersection_2.java
@@ -1,13 +1,50 @@
 package org.algorithm.interview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Auther: Ban
  * @Date: 2023/12/14 11:48
  * @Description: <p>
+ * 区间列表的交集
+ * <p>
+ * 给定两个由一些 闭区间 组成的列表，firstList 和 secondList ，其中 firstList[i] = [starti, endi] 而 secondList[j] = [startj, endj] 。
+ * 每个区间列表都是成对 不相交 的，并且 已经排序 。
+ * 返回这 两个区间列表的交集 。
+ * 形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <= b 。
+ * 两个闭区间的 交集 是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3] 。
  */
 public class IntervalIntersection_2 {
-    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        // 三种情况
-        return null;
+
+    /**
+     * 两个区间无交集 b2 < a1 or a2 < b1
+     * 两个区间存在交集 b2 >= a1 and a2 >= b1
+     */
+    public int[][] intervalIntersection(int[][] f, int[][] s) {
+        List<int[]> res = new ArrayList<>();
+        // 双指针
+        int i = 0, j = 0;
+        while (i < f.length && j < s.length) {
+            // 两个区间[a1,a2],[b1][b2]
+            int a1 = f[i][0];
+            int a2 = f[i][1];
+            int b1 = s[j][0];
+            int b2 = s[j][1];
+            // 两个区间存在交集 b2 >= a1 and a2 >= b1
+            if (b2 >= a1 && a2 >= b1) {
+                // 交集区间是 [c1, c2], c1 = max(a1, b1)，c2 = min(a2 ,b2)
+                int c1 = Math.max(a1, b1);
+                int c2 = Math.min(a2, b2);
+                res.add(new int[]{c1, c2});
+            }
+            //指针移动
+            if (a2 > b2) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
     }
 }
--- algorithm/interview/Merge_2.java
+++ algorithm/interview/Merge_2.java
@@ -8,6 +8,9 @@ import java.util.List;
  * @Auther: Ban
  * @Date: 2023/12/14 11:00
  * @Description: <p>
+ * 合并区间
+ * 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+ * 请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
  */
 public class Merge_2 {
     public int[][] merge(int[][] intervals) {
@@ -29,23 +32,53 @@ public class Merge_2 {
             if (left <= t0 && t1 <= right) {
                 continue;
             }
-            // 相交区间
+            // 相交区间,合并为大区间
             if (t0 <= right && right <= t1) {
+                // 更新大区间
                 right = t1;
             }
-            // 完全不相交
+            // 相邻区间，完全不相交
             if (right < t0) {
+                // 添加上一个区间
                 list.add(new int[]{left, right});
                 left = t0;
                 right = t1;
             }
         }
+        // 添加最后一个区间
         list.add(new int[]{left, right});
-        int[][] ans = new int[list.size()][2];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = list.get(i);
+
+        return list.toArray(new int[list.size()][]);
+    }
+
+    public int[][] merge2(int[][] intervals) {
+        // 排序
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - b[1]; // 终点降序
+            }
+            return a[0] - b[0]; // 起点升序
+        });
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            // 相邻区间进行判断
+            // 当前区间
+            int[] cur = intervals[i];
+            // 前一个区间，res 中最后一个元素的引用
+            int[] last = res.get(res.size() - 1);
+
+            // 区间 相交 或者 覆盖，合并为大区间
+            if (cur[0] <= last[1]) {
+                // 合并，更新 last，去最大值
+                last[1] = Math.max(cur[1], last[1]);
+            }
+            // 区间完全不相交，加入res
+            else {
+                res.add(cur);
+            }
         }
-        return ans;
+        return res.toArray(new int[res.size()][]);
     }
 
 }
